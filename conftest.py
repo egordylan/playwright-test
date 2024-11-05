9add2bff97f326ced140c5689b700e03bb0ac25c
egordylan <egordylan@gmail.com>
2024-11-05 22:38:44 +0300

    feat(playwright): Add try/except to toggle PASS between local and remote

--- conftest.py
+++ conftest.py
@@ -2,8 +2,13 @@ import os
 import time
 from playwright.sync_api import Playwright, expect
 import pytest
-# import utils.secret_config
 
+
+try:
+    PASSWORD = os.environ["PASSWORD"]
+except KeyError:
+    import utils.secret_config
+    PASSWORD = utils.secret_config.PASSWORD
 
 # @pytest.fixture(scope="session")
 # def set_up(browser):
@@ -51,7 +56,7 @@ def login_set_up(set_up):
     page.fill("input:below(:text('Email'))", "kaneki@gmail.com")
     page.get_by_test_id("emailAuth").get_by_label("Email").press("Tab")
     # page.get_by_label("Password").fill(utils.secret_config.PASSWORD)
-    page.get_by_label("Password").fill(os.environ["PASSWORD"])
+    page.get_by_label("Password").fill(PASSWORD)
     page.get_by_test_id("submit").get_by_test_id("buttonElement").click()
     page.wait_for_load_state("networkidle")
 
